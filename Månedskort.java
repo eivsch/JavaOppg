92a33a31779bdf2e1e7f6b341b3e677c0c784896
eivsch <s198752@stud.hioa.no>
2013-11-11 12:50:34 +0100

    ny

---
+++ Månedskort.java
@@ -0,0 +1,28 @@
+/*
+
+Programmering høst 2013
+Obligatorsik Oppgave 4
+Oppgave 1
+
+Gruppemedlemer:
+Eivind Schulstad	(s198752)
+Gretar Ævarsson		(s198586)
+Sigurd Hølleland	(s198597)
+
+*/
+
+
+public class Månedskort < arver Reisekort  >
+{
+  public final static int MÅNEDSPRIS = 1040;
+  <  private datafelt  >
+
+  public Månedskort() { ... }
+
+  public static int getSumAlleMånedskort() { ... }
+
+  public static int getAntallSolgte() { ... }
+
+  public boolean gyldig() { ... }  // redefinert
+
+}  // end of class Månedskort
